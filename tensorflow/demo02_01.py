31384f90f35cc27b61f41f7427c0a76974f59f6e
xuyuandong <17565912@qq.com>
2018-03-29 09:23:05 +0800

    识别

--- tensorflow/demo02_01.py
+++ tensorflow/demo02_01.py
@@ -80,7 +80,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 saver = tf.train.Saver()
 
 sess.run(tf.global_variables_initializer())
-for i in range(20000):
+for i in range(10000):
     batch = mnist.train.next_batch(50)
     if i % 100 == 0:
         train_accuracy = accuracy.eval(feed_dict={
@@ -88,7 +88,7 @@ for i in range(20000):
         print("步长 %d, 训练使用时长 %g" % (i, train_accuracy))
 
     train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
-saver.save(sess, '/home/XXOO/learning_tensorflow/form/model.ckpt')  # 保存模型参数，注意把这里改为自己的路径
+saver.save(sess, '/Users/xuyuandong/Desktop/model/model.ckpt')  # 保存模型参数，注意把这里改为自己的路径
 
 print("test accuracy %g" % accuracy.eval(feed_dict={
     x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
